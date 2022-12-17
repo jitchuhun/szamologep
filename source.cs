7336feaa50823d94cbca9495862622d2a9a882be
Jitchu <115893541+jitchuhun@users.noreply.github.com>
2022-12-17 17:27:21 +0100

    szamologepv2.1
    v2.1:
    - char tombbe muveletek
    - kovetkezo update: atlathatobb kod, hatvanyozas
    
    v2:
    - negyzetgyok vonas

--- source.cs
+++ source.cs
@@ -14,67 +14,70 @@ namespace szamologep
             Console.WriteLine("Számológép (by: DM)");
             Console.WriteLine("...");
             bool kilepes;
+            char[] muveletek = new char[5] {'+','-','*','/','N'};
             do
             {
                 Console.WriteLine("+ vagy - vagy * vagy / vagy N");
                 Console.WriteLine("Művelet választása:");
                 char muvelet = Convert.ToChar(Console.ReadLine());
 
-                if (muvelet != '+' && muvelet != '-' && muvelet != '*' && muvelet != '/' && muvelet != 'N')
+                if (muveletek.Contains(muvelet))
                 {
-                    Console.WriteLine("Ilyen műveletet nem tudok elvégezni");
-                }
-                
-                if (muvelet == 'N')
-                {
-                    Console.WriteLine("Gépelje be a számot:");
-                    double x = Convert.ToDouble(Console.ReadLine());
-
-                    switch (muvelet)
+                    if (muvelet == 'N')
                     {
-                        case 'N':
-                            if (x >= 0)
-                            {
-                                Console.WriteLine(Math.Sqrt(x));
-                            }
-                            else if (x < 0)
-                            {
-                                Console.WriteLine("Nem értelmezhető");
-                            }
-                            break;
+                        Console.WriteLine("Gépelje be a számot:");
+                        double x = Convert.ToDouble(Console.ReadLine());
+
+                        switch (muvelet)
+                        {
+                            case 'N':
+                                if (x >= 0)
+                                {
+                                    Console.WriteLine(Math.Sqrt(x));
+                                }
+                                else if (x < 0)
+                                {
+                                    Console.WriteLine("Nem értelmezhető");
+                                }
+                                break;
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Gépelje be az 1. számot:");
+                        double x = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("Gépelje be a 2. számot:");
+                        double y = Convert.ToDouble(Console.ReadLine());
+
+                        switch (muvelet)
+                        {
+                            case '+':
+                                Console.WriteLine("Az összeg: {0}", x + y);
+                                break;
+                            case '-':
+                                Console.WriteLine("A különbség: {0}", x - y);
+                                break;
+                            case '*':
+                                Console.WriteLine("A szorzat: {0}", x * y);
+                                break;
+                            case '/':
+                                if (y != 0)
+                                {
+                                    Console.WriteLine("A hányados: {0}", x / y);
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Nem értelmezhető.");
+                                }
+                                break;
+                        }
                     }
                 }
                 else
                 {
-                    Console.WriteLine("Gépelje be az 1. számot:");
-                    double x = Convert.ToDouble(Console.ReadLine());
-                    Console.WriteLine("Gépelje be a 2. számot:");
-                    double y = Convert.ToDouble(Console.ReadLine());
-
-                    switch (muvelet)
-                    {
-                        case '+':
-                            Console.WriteLine("Az összeg: {0}", x + y);
-                            break;
-                        case '-':
-                            Console.WriteLine("A különbség: {0}", x - y);
-                            break;
-                        case '*':
-                            Console.WriteLine("A szorzat: {0}", x * y);
-                            break;
-                        case '/':
-                            if (y != 0)
-                            {
-                                Console.WriteLine("A hányados: {0}", x / y);
-                            }
-                            else
-                            {
-                                Console.WriteLine("Nem értelmezhető.");
-                            }
-                            break;
-                    }
+                    Console.WriteLine("Ilyen muveletet nem tudok elvegezni");
                 }
-
+                
                 Console.WriteLine("Ki szeretnél lépni? (true/false)");
                 kilepes = Convert.ToBoolean(Console.ReadLine());
                 Console.WriteLine("...");
